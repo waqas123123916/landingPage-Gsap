f45eaf353c8a16a1b8367585a0b982053403aecf
Waqas12344 <wm635950@gmail.com>
2025-07-16 13:11:44 +0500

    complete setup , add navbar ,and hero section apply gsap animation on it

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import gsap from "gsap"
+import { ScrollTrigger,SplitText } from "gsap/all"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+gsap.registerPlugin(ScrollTrigger, SplitText)
+function App() {
+  return (
+     <main>
+      <Navbar/>
+     <Hero/>
+     <div className="h-dvh bg-black"></div>
+     </main>
+  )
+}
+
+export default App
