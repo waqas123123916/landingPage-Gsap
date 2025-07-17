0a74fc1d1e0bae2c815a0b4f3b9cf3c2c786301f
waqas123123916 <waqas@cosmicsync.co>
2025-07-17 10:40:52 +0500

    Add Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Cocktails from "./components/Cocktails"
 import About from "./components/About"
 import Art from "./components/Art"
 import Menu from "./components/Menu"
+import Contact from "./components/Contact"
 gsap.registerPlugin(ScrollTrigger, SplitText)
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <About/>
       <Art/>
       <Menu/>
+      <Contact/>
      </main>
   )
 }
